38a8ed73f92faa63d4449cece2521cdec11aa669
meni181818 <meni181818@gmail.com>
2014-05-19 01:21:54 +0300

    Update remove-commands.bat

--- remove-commands.bat
+++ remove-commands.bat
@@ -1,9 +1,12 @@
 REM === removing service ===
-REM for example, stopping and deleting the service "AdobeFlashPlayerUpdateSvc".
-sc stop AdobeFlashPlayerUpdateSvc
-sc delete AdobeFlashPlayerUpdateSvc
+REM for example, stopping and deleting the service "AdobeFlashPlayerUpdateSvc":
+sc stop AdobeFlashPlayerUpdateSvc :: stopping the service
+sc delete AdobeFlashPlayerUpdateSvc :: deleting the service
 
 REM === removing Registry run value ===
-REM for example removing the value "Adobe ARM" from run key on registry.
+REM for example removing the value "Adobe ARM" from run key on registry:
 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v "Adobe ARM" /f
 
+REM === removing Scheduled Task ===
+REM for example removing the Scheduled Task "Adobe Flash Player Updater":
+schtasks /delete /tn "Adobe Flash Player Updater" /f
