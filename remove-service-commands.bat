ba5292cf52ed382d4f77228c4aa206b48ae766df
meni181818 <meni181818@gmail.com>
2014-05-19 01:05:10 +0300

    Update remove-service-commands.bat

--- remove-service-commands.bat
+++ remove-service-commands.bat
@@ -1,3 +1,3 @@
-REM for example, stoping and deleteing the service "AdobeFlashPlayerUpdateSvc".
+REM for example, stopping and deleting the service "AdobeFlashPlayerUpdateSvc".
 sc stop AdobeFlashPlayerUpdateSvc
 sc delete AdobeFlashPlayerUpdateSvc
