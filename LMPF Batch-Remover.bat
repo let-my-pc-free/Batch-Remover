7a5536c039bf3a10604894aeb7cd3e4caf716d99
meni181818 <meni181818@gmail.com>
2014-05-19 22:25:30 +0300

    Update LMPF Batch-Remover.bat

--- LMPF Batch-Remover.bat
+++ LMPF Batch-Remover.bat
@@ -4,18 +4,18 @@
 pushd %~dp0
 
 :: for each line in unwanted-services.txt, stop and delete the service name.
-FOR /F "delims=;" %%s IN (unwanted-services.txt) DO (
+FOR /F "delims=;" %%s IN (unwanted-lists\unwanted-services.txt) DO (
 sc stop %%s
 sc delete %%s
 )
 
 :: for each line in unwanted-Run_Registry.txt, delete the value from run key on registry.
-FOR /F "delims=;" %%r IN (unwanted-Run_Registry.txt) DO (
+FOR /F "delims=;" %%r IN (unwanted-lists\unwanted-Run_Registry.txt) DO (
 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v %%r /f
 )
 
 :: for each line in unwanted-Scheduled_Tasks-list.txt, delete the Scheduled Task.
-FOR /F "delims=;" %%t IN (unwanted-Scheduled_Tasks-list.txt) DO (
+FOR /F "delims=;" %%t IN (unwanted-lists\unwanted-Scheduled_Tasks-list.txt) DO (
 schtasks /delete /tn %%t /f
 )
 
